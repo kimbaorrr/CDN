d7529594dcf5d4438684b7d1b8568041744e087e
kimbaorrr <57533292+kimbaorrr@users.noreply.github.com>
2022-02-12 22:36:09 +0700

    Delete main.js

--- main.js
+++
@@ -1,1 +0,0 @@
-var devtools=function(){};devtools.toString=function(){return window.location.replace("about:blank"),"-"},setInterval(()=>{console.profile(devtools),console.profileEnd(devtools)},1e3);
