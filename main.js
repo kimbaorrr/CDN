0bdc9087b6c976a3a15c3c01d465b4e2cbd5edab
kimbaorrr <57533292+kimbaorrr@users.noreply.github.com>
2022-02-12 22:06:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+var devtools=function(){};devtools.toString=function(){return alert("Please close DevTools to continue..."),"-"},setInterval(()=>{console.profile(devtools),console.profileEnd(devtools)},1e3);
