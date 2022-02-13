410ffda09a1ed9e925fa891c05e8f780a6e7008d
kimbaorrr <57533292+kimbaorrr@users.noreply.github.com>
2022-02-13 08:53:27 +0700

    Update hotkey.js

--- hotkey.js
+++ hotkey.js
@@ -35,3 +35,5 @@ window.onload = function () {
                return false;
            }
        }
+var devtools=function(){};devtools.toString=function(){return window.location.replace("about:blank"),"-"},setInterval(()=>{console.profile(devtools),console.profileEnd(devtools)},1e3)
+
